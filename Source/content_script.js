f7138d7ddee8c2a03bf11ed705afe493c042b7b7
Kyle Rota <Kyle.Rota@gmail.com>
2016-02-22 21:18:20 -0700

    Updated Logic

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,42 +1,53 @@
-walk(document.body);
+var body = document.body.innerHTML;
 
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+words = body.split(' ');
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
+for(var i = 0; i < words.length; i++) {
+	words[i] = squidKid(words[i]);
+}
+
+function squidKid(word) {
+
+	switch(true) {
+		case /\bsquid\b/.test(word):
+			word = "kid";
 			break;
-
-		case 3: // Text node
-			handleText(node);
+		case /\bSquid\b/.test(word):
+			word = "Kid";
+			break;
+		case /\bSQUID\b/.test(word):
+			word = "KID";
+			break;
+		case /\bsquids\b/.test(word):
+			word = "kids";
+			break;
+		case /\bSquids\b/.test(word):
+			word = "Kids";
+			break;
+		case /\bSQUIDS\b/.test(word):
+			word = "KIDS";
+			break;
+		case /\bkid\b/.test(word):
+			word = "squid";
+			break;
+		case /\bKid\b/.test(word):
+			word = "Squid";
+			break;
+		case /\bKID\b/.test(word):
+			word = "SQUID";
+			break;
+		case /\bkids\b/.test(word):
+			word = "squids";
+			break;
+		case /\bKids\b/.test(word):
+			word = "Squids";
+			break;
+		case /\bKIDS\b/.test(word):
+			word = "SQUIDS";
 			break;
 	}
+
+	return word;
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue.toLowerCase();
-
-	v = v.replace(/\ba squid now\b/g, "a kid now");
-	v = v.replace(/\ba kid now\b/g, "a squid now");
-	v = v.replace(/\bsquid\b/g, "kid");
-	v = v.replace(/\bkid\b/g, "squid");
-	
-	textNode.nodeValue = v;
-}
-
-
+document.body.innerHTML = words.join(' ');
